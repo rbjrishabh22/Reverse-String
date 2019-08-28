0eabab44bf04f6754b210b62eaa9e090fafbea88
Rishabh Bhardwaj <rbjrishabh22@gmail.com>
2019-08-29 00:09:03 +0530

    Reverse String

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strValue = "India is my country";
+            string rev = "";
+            int l = strValue.Length - 1;
+            while (l >= 0)
+            {
+                rev = rev + strValue[l];
+                l--;
+            }
+            Console.WriteLine("Befor reverse: {0}", strValue);
+            Console.WriteLine("After reverse: {0}",rev);
+            Console.ReadLine();
+        }
+    }
+}
